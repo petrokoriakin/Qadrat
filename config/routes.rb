e7983dab18e2c110a60183fc725abf0eae6b2152
malworm <snowbars125@gmail.com>
2016-03-31 21:42:09 +0300

    Cleaning up the code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # devise_for :users
 
   resources :posts do
     resources :comments
@@ -11,10 +10,5 @@ Rails.application.routes.draw do
 
   get '/about' => 'pages#about'
   get 'userposts/:id', to: 'posts#userposts', as: :userposts
-  # get 'users/:id' => 'users#show'
   resources :users, only: [:show, :update, :edit]
-  # get '/user/:id', to: 'pages#profile'
-  # get '/myprofile', to: 'pages#myprofile'
-  # get '/userinfo', to: 'pages#userinfo'
-  # post '/userinfo', to: 'pages#userinfo'
 end
